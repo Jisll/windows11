fcee427721c222cf46e44076da9cd6708850ac34
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 00:59:23 +0200

    Update Clean Windows.cmd

--- Additional/Clean Windows.cmd
+++ Additional/Clean Windows.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
+echo Script By Jisll
 
 :: Check if the script is run with administrative privileges
 net session >nul 2>&1
@@ -10,27 +11,17 @@ if %errorlevel% neq 0 (
 )
 
 :: List of folders to clean
-set "folders_to_clean=%WinDir%\Temp %WinDir%\Prefetch %Temp% %AppData%\Temp %HomePath%\AppData\LocalLow\Temp"
-
-:: List of drivers folders (if needed)
-:: set "drivers_folders=%SYSTEMDRIVE%\AMD %SYSTEMDRIVE%\NVIDIA %SYSTEMDRIVE%\INTEL"
+set "folders_to_clean=%WinDir%\Temp %WinDir%\Prefetch %Temp% %AppData%\Temp %LocalAppData%\Temp"
 
 echo Cleaning temporary files and folders...
 
 :: Clean temporary files and folders
 for %%F in (%folders_to_clean%) do (
     echo Deleting files in %%F
-    del /s /f /q "%%F\*.*"
-    rd /s /q "%%F"
-    md "%%F"
+    del /s /f /q "%%F\*.*" 2>nul
+    rd /s /q "%%F" 2>nul
+    mkdir "%%F"
 )
-
-:: Clean drivers folders (if needed)
-:: for %%D in (%drivers_folders%) do (
-::     echo Deleting files in %%D
-::     del /s /f /q "%%D\*.*"
-::     rd /s /q "%%D"
-:: )
 
 echo.
 echo Windows Clean Up Done! You can exit by pressing any key.
