e0a1067b63398d667c1734951cf40eeebd57de6b
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 22:02:26 +0200

    Update Optimize Windows.cmd

--- Optimize Windows.cmd
+++ Optimize Windows.cmd
@@ -2,6 +2,7 @@
 setlocal EnableDelayedExpansion
 color 17
 cls
+
 echo.
 echo.
 echo    ____        _      __    ___            ___      ____
@@ -16,26 +17,27 @@ echo This script will modify various settings. Continue only if you understand t
 echo.
 set /p "continue=Press ENTER to start the process..."
 
-REM Define the services to configure
+REM Services
 set "services_manual=ALG AppIDSvc AppMgmt AppReadiness AppXSvc Appinfo AxInstSV BDESVC BITS BTAGService BcastDVRUserService_dc2a4 BluetoothUserService_dc2a4 Browser CDPSvc COMSysApp CaptureService_dc2a4 CertPropSvc ClipSVC ConsentUxUserSvc_dc2a4 CredentialEnrollmentManagerUserSvc_dc2a4 CscService DcpSvc DevQueryBroker DeviceAssociationBrokerSvc_dc2a4 DeviceAssociationService DeviceInstall DevicePickerUserSvc_dc2a4 DevicesFlowUserSvc_dc2a4 DisplayEnhancementService DmEnrollmentSvc DoSvc DsSvc DsmSvc EFS EapHost EntAppSvc FDResPub Fax FrameServer FrameServerMonitor GraphicsPerfSvc HomeGroupListener HomeGroupProvider HvHost IEEtwCollectorService IKEEXT InstallService InventorySvc IpxlatCfgSvc KtmRm LicenseManager LxpSvc MSDTC MSiSCSI MapsBroker McpManagementService MessagingService_dc2a4 MicrosoftEdgeElevationService MixedRealityOpenXRSvc NPSMSvc_dc2a4 NaturalAuthentication NcaSvc NcbService NcdAutoSetup NetSetupSvc Netlogon Netman NgcCtnrSvc NgcSvc NlaSvc P9RdrService_dc2a4 PNRPAutoReg PNRPsvc PcaSvc PeerDistSvc PenService_dc2a4 PerfHost PhoneSvc PimIndexMaintenanceSvc_dc2a4 PlugPlay PolicyAgent PrintNotify PrintWorkflowUserSvc_dc2a4 PushToInstall QWAVE RasAuto RasMan RetailDemo RmSvc RpcLocator SCPolicySvc SCardSvr SDRSVC SEMgrSvc SNMPTRAP SNMPTrap SSDPSRV ScDeviceEnum SecurityHealthService Sense SensorDataService SensorService SensrSvc SessionEnv SharedAccess SharedRealitySvc SmsRouter SstpSvc StateRepository StiSvc StorSvc TabletInputService TapiSrv TextInputManagementService TieringEngineService TimeBroker TimeBrokerSvc TokenBroker TroubleshootingSvc TrustedInstaller UI0Detect UdkUserSvc_dc2a4 UmRdpService UnistoreSvc_dc2a4 UserDataSvc_dc2a4 UsoSvc VSS VacSvc W32Time WEPHOSTSVC WFDSConMgrSvc WMPNetworkSvc WManSvc WPDBusEnum WSService WSearch WaaSMedicSvc WalletService WarpJITSvc WbioSrvc WcsPlugInService WdNisSvc WdiServiceHost WdiSystemHost WebClient Wecsvc WerSvc WiaRpc WinHttpAutoProxySvc WinRM WpcMonSvc WpnService WwanSvc XblAuthManager XblGameSave XboxGipSvc XboxNetApiSvc autotimesvc bthserv camsvc cbdhsvc_dc2a4 cloudidsvc dcsvc defragsvc diagnosticshub.standardcollector.service diagsvc dmwappushservice dot3svc edgeupdate edgeupdatem embeddedmode fdPHost fhsvc hidserv icssvc lfsvc lltdsvc lmhosts msiserver netprofm p2pimsvc p2psvc perceptionsimulation pla seclogon smphost spectrum sppsvc svsvc swprv upnphost vds vm3dservice vmicguestinterface vmicheartbeat vmickvpexchange vmicrdv vmicshutdown vmictimesync vmicvmsession vmicvss vmvss wbengine wcncsvc webthreatdefsvc wercplsupport wisvc wlidsvc wlpasvc wmiApSrv workfolderssvc wscsvc wuauserv wudfsvc"
-
 set "services_auto=AudioEndpointBuilder AudioSrv Audiosrv BFE BrokerInfrastructure BthAvctpSvc BthHFSrv CDPUserSvc_dc2a4 CoreMessagingRegistrar CryptSvc DPS DcomLaunch Dhcp DispBrokerDesktopSvc Dnscache DusmSvc EventLog EventSystem FontCache KeyIso LSM LanmanServer LanmanWorkstation MpsSvc OneSyncSvc_dc2a4 Power ProfSvc RpcEptMapper RpcSs SENS SamSs Schedule SgrmBroker ShellHWDetection Spooler SysMain SystemEventsBroker TermService Themes TrkWks UserManager VGAuthService VMTools VaultSvc Wcmsvc WinDefend Winmgmt WlanSvc WpnUserService_dc2a4 gpsvc iphlpsvc mpssvc nsi tiledatamodelsvc webthreatdefusersvc_dc2a4"
-
 set "services_disabled=AJRouter AppVClient AssignedAccessManagerSvc DiagTrack DialogBlockingService MsKeyboardFilter NetTcpPortSharing RemoteAccess RemoteRegistry UevAgentService shpamsvc ssh-agent tzautoupdate uhssvc"
 
-REM Loop through the services and configure them
-for %%s in (%services_manual%) do (
-    sc config "%%s" start= demand
-    echo "%%s" set to Manual
-)
-for %%s in (%services_auto%) do (
-    sc config "%%s" start= auto
-    echo "%%s" set to Automatic
-)
-for %%s in (%services_disabled%) do (
-    sc config "%%s" start= disabled
-    echo "%%s" set to Disabled
-)
+for %%s in (%services_manual%) do sc config "%%s" start= demand && echo "%%s" set to Manual
+for %%s in (%services_auto%) do sc config "%%s" start= auto && echo "%%s" set to Automatic
+for %%s in (%services_disabled%) do sc config "%%s" start= disabled && echo "%%s" set to Disabled
+
+REM Disable Telemetry Tasks
+for %%t in (
+    "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+    "Microsoft\Windows\Application Experience\ProgramDataUpdater"
+    "Microsoft\Windows\Autochk\Proxy"
+    "Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
+    "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"
+    "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector"
+    "Microsoft\Windows\Feedback\Siuf\DmClient"
+    "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload"
+    "Microsoft\Windows\Windows Error Reporting\QueueReporting"
+) do schtasks /Change /TN "%%t" /Disable
 
 REM Disable Telemetry Tasks
 schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
@@ -59,22 +61,12 @@ REM Change boot menu policy to Legacy
 bcdedit /set {current} bootmenupolicy Legacy
 
 REM Check Windows build version
-setlocal enabledelayedexpansion
-for /f "tokens=3" %%i in ('ver') do (
-    set "version=%%i"
-)
+for /f "tokens=3" %%i in ('ver') do set "version=%%i"
 if %version% lss 22557 (
     start "" taskmgr.exe
-
     :wait
     timeout /t 1 >nul
-    tasklist /FI "IMAGENAME eq taskmgr.exe" | find /i "taskmgr.exe" >nul
-    if errorlevel 1 (
-        goto :continue
-    ) else (
-        taskkill /f /im taskmgr.exe
-        goto :wait
-    )
+    tasklist /FI "IMAGENAME eq taskmgr.exe" | find /i "taskmgr.exe" >nul && (taskkill /f /im taskmgr.exe && goto :wait) || goto :continue
 )
 
 :continue
