e97562ffbeef430a11703c8a00712a466aaf8cdd
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 18:24:15 +0200

    Update Clean Settings.cmd

--- Personalization/Clean Settings.cmd
+++ Personalization/Clean Settings.cmd
@@ -1,6 +1,25 @@
 @echo off
-echo Script by Jisll
+setlocal EnableDelayedExpansion
+color 17
+cls
+echo.
+echo.
+echo    ____        _      __    ___            ___      ____
+echo   / __/_______(_)__  / /_  / _ )__ __  __ / (_)__  / / /
+echo  _\ \/ __/ __/ / _ \/ __/ / _  / // / / // / (_- // / /
+echo /___/\__/_/ /_/ .__/\__/ /____/\_, /  \___/_/__ /_ /_/
+echo              /_/              /___/
+echo.
+echo.
+echo Before proceeding, make sure to create a backup of your system.
+echo This script will modify various settings. Continue only if you understand the changes.
+echo.
+set /p "continue=Press ENTER to start the process..."
+
 echo Applying Clean Settings...
+
+:: Disable online tips
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v AllowOnlineTips /t REG_DWORD /d 0 /f
+
 echo Clean Settings applied.
 pause
