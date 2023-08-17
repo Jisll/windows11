918e92524dc65c0d5a71eb0f599228f254ab76a7
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 18:17:07 +0200

    Update Disable Services.cmd

--- Debloating/Disable Services.cmd
+++ Debloating/Disable Services.cmd
@@ -1,3 +1,21 @@
+@echo off
+setlocal EnableDelayedExpansion
+color 17
+cls
+echo.
+echo.
+echo    ____        _      __    ___            ___      ____
+echo   / __/_______(_)__  / /_  / _ )__ __  __ / (_)__  / / /
+echo  _\ \/ __/ __/ / _ \/ __/ / _  / // / / // / (_- // / /
+echo /___/\__/_/ /_/ .__/\__/ /____/\_, /  \___/_/__ /_ /_/
+echo              /_/              /___/
+echo.
+echo.
+echo Before proceeding, make sure to create a backup of your system.
+echo This script will modify various settings. Continue only if you understand the changes.
+echo.
+set /p "continue=Press ENTER to start the process..."
+
 :: Disable Storage Sense
 REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense" /f
 
