26a8efe52540ca4966d78bcc4d9f7634d6f6fca8
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 18:26:02 +0200

    Update Prevent Wallpaper Changes.cmd

--- Personalization/Prevent Wallpaper Changes.cmd
+++ Personalization/Prevent Wallpaper Changes.cmd
@@ -1,8 +1,30 @@
 @echo off
-echo Script by Jisll
-setlocal
+setlocal EnableDelayedExpansion
+color 17
+cls
+echo.
+echo.
+echo    ____        _      __    ___            ___      ____
+echo   / __/_______(_)__  / /_  / _ )__ __  __ / (_)__  / / /
+echo  _\ \/ __/ __/ / _ \/ __/ / _  / // / / // / (_- // / /
+echo /___/\__/_/ /_/ .__/\__/ /____/\_, /  \___/_/__ /_ /_/
+echo              /_/              /___/
+echo.
+echo.
+echo Before proceeding, make sure to create a backup of your system.
+echo This script will modify various settings. Continue only if you understand the changes.
+echo.
+set /p "continue=Press ENTER to start the process..."
+
 echo Applying Prevent Wallpaper Changes...
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoChangingWallPaper /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoChangingWallPaper /t REG_DWORD /d 1 /f
+
+:: Define Registry Keys
+set "RegKeyHKCU=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop"
+set "RegKeyHKLM=HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop"
+
+:: Prevent wallpaper changes
+reg add "%RegKeyHKCU%" /v NoChangingWallPaper /t REG_DWORD /d 1 /f
+reg add "%RegKeyHKLM%" /v NoChangingWallPaper /t REG_DWORD /d 1 /f
+
 echo Prevent Wallpaper Changes applied.
 pause
