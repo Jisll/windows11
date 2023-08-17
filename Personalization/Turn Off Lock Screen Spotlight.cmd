ef2c393c36c179796f6703d1f28e21ab2bc421b4
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 18:26:48 +0200

    Update Turn Off Lock Screen Spotlight.cmd

--- Personalization/Turn Off Lock Screen Spotlight.cmd
+++ Personalization/Turn Off Lock Screen Spotlight.cmd
@@ -1,16 +1,34 @@
 @echo off
-echo Script by Jisll
+setlocal EnableDelayedExpansion
+color 17
+cls
+echo.
+echo.
+echo    ____        _      __    ___            ___      ____
+echo   / __/_______(_)__  / /_  / _ )__ __  __ / (_)__  / / /
+echo  _\ \/ __/ __/ / _ \/ __/ / _  / // / / // / (_- // / /
+echo /___/\__/_/ /_/ .__/\__/ /____/\_, /  \___/_/__ /_ /_/
+echo              /_/              /___/
+echo.
+echo.
+echo Before proceeding, make sure to create a backup of your system.
+echo This script will modify various settings. Continue only if you understand the changes.
+echo.
+set /p "continue=Press ENTER to start the process..."
+
 echo Applying Turn Off Lock Screen Spotlight...
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightWindowsWelcomeExperience /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v NoChangingLockScreen /t REG_DWORD /d 0 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightFeatures /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightOnActionCenter /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightOnSettings /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableThirdPartySuggestions /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v ConfigureWindowsSpotlight /t REG_DWORD /d 2 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v IncludeEnterpriseSpotlight /t REG_DWORD /d 0 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "RotatingLockScreenEnabled" /t REG_DWORD /d 0 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "LRotatingLockScreenEnabled" /t REG_DWORD /d 0 /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "RotatingLockScreenOverlayEnabled" /t REG_DWORD /d 0 /f
+
+:: Define Registry Keys
+set "RegKeyCloudContent=HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+set "RegKeyPersonalization=HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization"
+
+:: Turn off Lock Screen Spotlight
+reg add "%RegKeyCloudContent%" /v DisableWindowsSpotlightWindowsWelcomeExperience /t REG_DWORD /d 1 /f
+reg add "%RegKeyPersonalization%" /v NoChangingLockScreen /t REG_DWORD /d 0 /f
+reg add "%RegKeyCloudContent%" /v DisableWindowsSpotlightFeatures /t REG_DWORD /d 1 /f
+reg add "%RegKeyCloudContent%" /v DisableWindowsSpotlightOnActionCenter /t REG_DWORD /d 1 /f
+reg add "%RegKeyCloudContent%" /v DisableWindowsSpotlightOnSettings /t REG_DWORD /d 1 /f
+reg add "%RegKeyCloudContent%" /v DisableThirdPartySuggestions /t REG_DWORD /d 1 /f
+
 echo Turn Off Lock Screen Spotlight applied.
 pause
