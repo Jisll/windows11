0a1f25af21fae808ad73e45078aad8a86b5c4d7b
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 18:12:04 +0200

    Update Uninstall or Deactivate Microsoft Edge.cmd

--- Apps/Uninstall or Deactivate Microsoft Edge.cmd
+++ Apps/Uninstall or Deactivate Microsoft Edge.cmd
@@ -1,6 +1,26 @@
 @echo off
-echo Script by Jisll
-echo Applying changes to the registry...
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v "1" /d "msedge.exe" /f
+setlocal EnableDelayedExpansion
+color 17
+cls
+echo.
+echo.
+echo    ____        _      __    ___            ___      ____
+echo   / __/_______(_)__  / /_  / _ )__ __  __ / (_)__  / / /
+echo  _\ \/ __/ __/ / _ \/ __/ / _  / // / / // / (_- // / /
+echo /___/\__/_/ /_/ .__/\__/ /____/\_, /  \___/_/__ /_ /_/
+echo              /_/              /___/
+echo.
+echo.
+echo Before proceeding, make sure to create a backup of your system.
+echo This script will modify various settings. Continue only if you understand the changes.
+echo.
+set /p "continue=Press ENTER to start the process..."
+
+:: Set common registry key string
+set "regKeyHKCU=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun"
+
+:: Add registry key to disallow running Microsoft Edge
+reg add "%regKeyHKCU%" /v "1" /d "msedge.exe" /f
+
 echo Registry changes applied.
 pause
