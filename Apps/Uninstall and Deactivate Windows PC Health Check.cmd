ce8530e66fe25e412c78769762fd82c4e1b75ceb
Maurice <69192557+Jisll@users.noreply.github.com>
2023-08-17 18:11:20 +0200

    Update Uninstall and Deactivate Windows PC Health Check.cmd

--- Apps/Uninstall and Deactivate Windows PC Health Check.cmd
+++ Apps/Uninstall and Deactivate Windows PC Health Check.cmd
@@ -1,7 +1,27 @@
 @echo off
-echo Script by Jisll
-echo Applying changes to the registry...
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PCHC" /v PreviousUninstall /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PCHealthCheck" /v installed /t REG_DWORD /d 1 /f
+setlocal EnableDelayedExpansion
+color 17
+cls
+echo.
+echo.
+echo    ____        _      __    ___            ___      ____
+echo   / __/_______(_)__  / /_  / _ )__ __  __ / (_)__  / / /
+echo  _\ \/ __/ __/ / _ \/ __/ / _  / // / / // / (_- // / /
+echo /___/\__/_/ /_/ .__/\__/ /____/\_, /  \___/_/__ /_ /_/
+echo              /_/              /___/
+echo.
+echo.
+echo Before proceeding, make sure to create a backup of your system.
+echo This script will modify various settings. Continue only if you understand the changes.
+echo.
+set /p "continue=Press ENTER to start the process..."
+
+:: Set common registry key strings
+set "regKeyHKLM=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft"
+
+:: Add registry keys to indicate previous uninstall and installation status
+reg add "%regKeyHKLM%\PCHC" /v PreviousUninstall /t REG_DWORD /d 1 /f
+reg add "%regKeyHKLM%\PCHealthCheck" /v installed /t REG_DWORD /d 1 /f
+
 echo Registry changes applied.
 pause
